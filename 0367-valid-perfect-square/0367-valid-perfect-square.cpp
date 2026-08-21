8cd0e1a7d99d2b4497ab33c2ed3f329f394aac6c
anand <achawan784@gmail.com>
2026-08-22 00:03:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.61%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num<2){
+            return true;
+        }
+        int left=1,right=num/2;
+        while(left<=right){
+            long long mid=left+(right-left)/2;
+            long int sq=mid*mid;
+            if(sq==num){
+                return true;
+            }
+            else if(sq<num){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            
+        }
+        return false;
+    }
+};
